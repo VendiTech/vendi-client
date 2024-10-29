30a88adaa56a721c3f3506267cc2cfee8559d91c
Adryian S <backend@venditech.com>
2024-10-29 10:15:50 +0100

    feat: add stacked bar chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,18 @@ import { LineChart } from '@/ui/atoms/LineChart';
 import { Card } from '@/ui/atoms/Card';
 import { Box, Typography } from '@mui/material';
 import { DoughnutChart } from '@/ui/atoms/DoughnutChart';
+import { StackedBarChart } from '@/ui/atoms/StackedBarChart';
 
 const chartData = [1, 0.8, 1.3, 1.1, 1.4, 2];
+const barsData = [
+  [2, 4],
+  [1, 5],
+  [2, 4],
+  [0.8, 3.3],
+  [2, 4],
+  [3, 4.2],
+  [4, 4.4],
+] 
 
 export default function Home() {
   const [openScheduleModal] = useCreateScheduleModal();
@@ -24,10 +34,51 @@ export default function Home() {
       <Button onClick={createSchedule}>modal</Button>
       <Link href={'/admin'}>Admin</Link>
 
-      <DoughnutChart sx={{
-        width: 200,
-        height: 200,
-      }} data={[234, 123, 632, 340, 65, 123]} total={3000}>
+      <Box sx={{ background: 'var(--gradient)', p: 3 }}>
+        <StackedBarChart
+          sx={{
+            width: 150,
+          }}
+          data={barsData}
+        />
+        <StackedBarChart
+          sx={{
+            width: 150,
+          }}
+          data={barsData}
+          variant={'good'}
+        />
+        <StackedBarChart
+          sx={{
+            width: 150,
+          }}
+          data={barsData}
+          variant={'bad'}
+        />
+        <StackedBarChart
+          sx={{
+            width: 150,
+          }}
+          placeholder
+          data={[
+            [0, 4],
+            [0, 5],
+            [0, 4],
+            [0, 3.3],
+            [0, 4],
+            [0, 4.2],
+            [0, 4.4],
+          ]}
+        />
+      </Box>
+
+      <DoughnutChart
+        sx={{
+          width: 200,
+          height: 200,
+        }}
+        data={[234, 123, 632, 340, 65, 123]}
+        total={3000}>
         <Typography sx={{ m: '0 auto' }}>16.3%</Typography>
       </DoughnutChart>
 
@@ -69,8 +120,8 @@ export default function Home() {
             height: 100,
           }}
         />
-          
-          <DoughnutChart
+
+        <DoughnutChart
           data={[27]}
           total={100}
           colors={['#ffffff']}
@@ -78,9 +129,12 @@ export default function Home() {
           sx={{
             width: 100,
             height: 100,
-          }}
-        >
-          <Typography variant={'lg-medium'} sx={{ m: '0 auto', color: '#ffffff' }}>27%</Typography>
+          }}>
+          <Typography
+            variant={'lg-medium'}
+            sx={{ m: '0 auto', color: '#ffffff' }}>
+            27%
+          </Typography>
         </DoughnutChart>
       </Box>
     </MainLayout>
---
+++ src/ui/atoms/StackedBarChart/StackedBarChart.tsx
@@ -0,0 +1,80 @@
+import React, { useLayoutEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart, BarElement, ChartData } from 'chart.js';
+import { Box, SxProps, Theme } from '@mui/material';
+
+Chart.register(BarElement);
+
+type Props = {
+  data: number[][];
+  sx?: SxProps<Theme>;
+  variant?: 'good' | 'bad';
+  placeholder?: boolean
+};
+
+const getCssVar = (name: string) =>
+  getComputedStyle(document.body).getPropertyValue(name);
+
+export const StackedBarChart = (props: Props) => {
+  const { data, variant, placeholder, sx } = props;
+
+  const [barColor, setBarColor] = useState('#ffffff');
+  const [backgroundColor, setBackgroundColor] = useState('#ffffff4d');
+  
+  useLayoutEffect(() => {
+    if (variant) {
+      setBackgroundColor(getCssVar('--slate-100'));
+    }
+    
+    if (variant === 'good') {
+      setBarColor(getCssVar('--green-500'));
+    }
+    
+    if (variant === 'bad') {
+      setBarColor(getCssVar('--red-500'));
+    }
+  }, [variant])
+  
+  const chartData: ChartData<'bar'> = {
+    labels: Array(data.length).fill(''),
+    datasets: [
+      {
+        label: '',
+        data: !placeholder ? data.map((item) => item[0]) : [],
+        backgroundColor: barColor,
+        barThickness: 10,
+        borderRadius: 3,
+      },
+      {
+        label: '',
+        data: data.map((item) => item[1]),
+        backgroundColor: backgroundColor,
+        barThickness: 10,
+        borderRadius: 3,
+      },
+    ],
+  };
+
+  return (
+    <Box sx={sx}>
+      <Bar
+        data={chartData}
+        options={{
+          devicePixelRatio: 2,
+          responsive: true,
+          scales: {
+            x: {
+              stacked: true,
+              display: false,
+            },
+            y: {
+              display: false,
+            },
+          },
+        }}
+      />
+    </Box>
+  );
+};
+
+export default StackedBarChart;
