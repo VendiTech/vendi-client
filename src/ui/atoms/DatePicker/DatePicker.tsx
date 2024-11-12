abfaaf30bc3bc6111f18a88c07cf3f53b8208465
Adryian S <backend@venditech.com>
2024-11-12 12:57:47 +0100

    fix: middleware build error

--- src/ui/atoms/DatePicker/DatePicker.tsx
+++ src/ui/atoms/DatePicker/DatePicker.tsx
@@ -1,7 +1,12 @@
-import { DatePicker as MuiDatePicker, DatePickerProps } from '@mui/x-date-pickers/DatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers';
+import {
+  DatePicker as MuiDatePicker,
+  DatePickerProps,
+} from '@mui/x-date-pickers/DatePicker';
+import { CalendarIcon, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Dayjs } from 'dayjs';
+import { InputField } from '@/ui/atoms/InputField';
+import { useState } from 'react';
 
 type Props = {
   placeholder?: string;
@@ -10,14 +15,43 @@ type Props = {
 export const DatePicker = (props: Props) => {
   const { placeholder, ...rest } = props;
 
+  const [open, setOpen] = useState(false);
+  
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <MuiDatePicker
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        slots={{
+          textField: InputField,
+        }}
         slotProps={{
+          popper: {
+            sx: {
+              '& .MuiPaper-root': {
+                border: '1px solid var(--slate-200)',
+                boxShadow: 'none',
+              },
+
+              '& .MuiPickersDay-root.Mui-selected': {
+                background: 'var(--sky-500)',
+              },
+            },
+          },
           textField: {
+            value: props.value,
+            onClick: () => setOpen(true),
             placeholder,
-            variant: 'outlined',
-            size: 'small',
+            sx: {
+              userSelect: 'none',
+            },
+            slotProps: {
+              
+            },
+            InputProps: {
+              endAdornment: <CalendarIcon width={16} height={16} />
+            },
           },
         }}
         {...rest}
--- src/ui/pages/DashboardPage/DashboardPage.tsx
+++ src/ui/pages/DashboardPage/DashboardPage.tsx
@@ -2,9 +2,14 @@
 
 import { MainLayout } from '@/ui/templates/MainLayout';
 import { DashboardTemplate } from '@/ui/templates/DashboardTemplate';
-import { GlobalFilters } from '@/ui/organisms/GlobalFilters';
+import { GlobalFilters, useGlobalFilters } from '@/ui/organisms/GlobalFilters';
+import { useGetSales } from '@/lib/api';
 
 export const DashboardPage = () => {
+  const { dateFrom, dateTo } = useGlobalFilters();
+
+  useGetSales({ dateFrom, dateTo });
+
   return (
     <MainLayout title={'Dashboard'} gap={2} actions={<GlobalFilters />}>
       <DashboardTemplate />
