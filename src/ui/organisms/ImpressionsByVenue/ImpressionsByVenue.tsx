0ed6c52debb030d58764ae917da3532ef86802a2
Adryian S <backend@venditech.com>
2024-11-20 15:04:00 +0100

    fix: build errors

--- src/ui/organisms/ImpressionsByVenue/ImpressionsByVenue.tsx
+++ src/ui/organisms/ImpressionsByVenue/ImpressionsByVenue.tsx
@@ -1,4 +1,3 @@
-import { useGlobalFilters } from '@/lib/services/GlobalFilters';
 import { ChartCard } from '@/ui/molecules/ChartCard';
 import { BarChart } from '@/ui/atoms/BarChart';
 
@@ -11,23 +10,12 @@ const data = [
   { label: 'Venue 6', value: 73 },
 ];
 
-const data1 = [
-  { label: 'Venue 1', value: 2 },
-  { label: 'Venue 2', value: 13 },
-  { label: 'Venue 3', value: 9 },
-  { label: 'Venue 4', value: 20 },
-  { label: 'Venue 5', value: 9 },
-  { label: 'Venue 6', value: 23 },
-];
-
 export const ImpressionsByVenue = () => {
-  const { region } = useGlobalFilters();
-
   return (
     <ChartCard
       title={'Impressions by venue over time'}
       subtitle={'Lorem ipsum dolor sit amet.'}>
-      <BarChart data={region === 'Liverpool' ? data : data1} />
+      <BarChart data={data} />
     </ChartCard>
   );
 };
