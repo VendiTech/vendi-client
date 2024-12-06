40eb3578706ae491a5e68bad35a5e39710ff39dd
Adryian S <backend@venditech.com>
2024-12-06 13:21:03 +0100

    fix: get statistic for previous month

--- src/lib/helpers/useStatisticDates.ts
+++ src/lib/helpers/useStatisticDates.ts
@@ -1,20 +1,18 @@
-import { useGlobalFilters } from '@/lib/services/GlobalFilters';
 import dayjs from 'dayjs';
+import { useGlobalFilters } from '@/lib/services/GlobalFilters';
 import { DATE_FORMAT } from '@/lib/constants/date';
 
 export const useStatisticDates = (getStatistic?: boolean) => {
   const { dateFrom, dateTo } = useGlobalFilters();
 
-  const dayjsDateFrom = dayjs(dateFrom, DATE_FORMAT);
-  const dayjsDateTo = dayjs(dateTo, DATE_FORMAT);
+  const previousMonth = dayjs(dateFrom, DATE_FORMAT).subtract(1, 'month');
 
   const startDate = getStatistic
-    ? dayjsDateFrom
-        .subtract(1 + dayjsDateTo.diff(dayjsDateFrom, 'day'), 'days')
-        .format(DATE_FORMAT)
+    ? previousMonth.startOf('month').format(DATE_FORMAT)
     : dateFrom;
+
   const endDate = getStatistic
-    ? dayjsDateFrom.subtract(1, 'days').format(DATE_FORMAT)
+    ? previousMonth.endOf('month').format(DATE_FORMAT)
     : dateTo;
 
   return {
